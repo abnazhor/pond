dd5fccea7b191d43c847033e44a74a09a47caa84
mbajur <mbajur@gmail.com>
2026-06-12 21:05:32 +0200

    Disable screenshoter for gifs

--- app/services/screenshoter.rb
+++ app/services/screenshoter.rb
@@ -3,6 +3,7 @@ class Screenshoter
   SCREEN_HEIGHT = 1440
 
   class ResponseStatusInvalidError < StandardError; end
+  class UnsupportedLinkType < StandardError; end
 
   def initialize(post)
     @post = post
@@ -10,9 +11,11 @@ class Screenshoter
 
   def call
     if @post.url.match?(%r{https?://(www\.)?instagram\.com/[a-zA-Z._]+})
-      raise NotImplementedError, "Instagram profile previews are not supported yet"
+      raise UnsupportedLinkType, "Instagram profile previews are not supported yet"
     elsif @post.url.match?(%r{https?://(www\.)?youtube\.com/watch\?v=[a-zA-Z0-9_-]+})
-      raise NotImplementedError, "YouTube video previews are not supported yet"
+      raise UnsupportedLinkType, "YouTube video previews are not supported yet"
+    elsif @post.url.match?(%r{https?://.*\.(gif|gifv)(\?.*)?$})
+      raise UnsupportedLinkType, "GIF screenshots are not supported, they make no sense"
     else
       Screenshoter::DefaultHandler.new(@post).call
     end
