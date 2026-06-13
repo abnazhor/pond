8a019936c24b4214a2a2c9137ac04fcd54ab52b7
mbajur <mbajur@gmail.com>
2026-06-13 10:13:20 +0200

    Limit GenerateScreenshotJob concurrency to 1

--- app/jobs/posts/generate_screenshot_job.rb
+++ app/jobs/posts/generate_screenshot_job.rb
@@ -1,6 +1,7 @@
 module Posts
   class GenerateScreenshotJob < ApplicationJob
-    queue_as :default
+    queue_as :screenshoter
+    limits_concurrency to: 1
 
     def perform(post)
       Screenshoter.new(post).call
