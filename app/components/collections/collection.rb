ca5baa8c4b27a9da81e3b27c31eaa3a6234e9eeb
mbajur <mbajur@gmail.com>
2026-06-14 18:22:02 +0200

    Cache collection component

--- app/components/collections/collection.rb
+++ app/components/collections/collection.rb
@@ -12,36 +12,38 @@ module Components
       end
 
       def view_template(&)
-        div(class: "mb-6", id: @id || dom_id(@collection)) do
-          div(class: "grid grid-cols-12 gap-6 w-full") do
-            div(class: "col-span-3") do
-              a(href: href, class: "hover:underline") do
-                render RubyUI::Heading(level: 4, class: "inline") { @presented_collection.name }
+        cache(@collection) do
+          div(class: "mb-6", id: @id || dom_id(@collection)) do
+            div(class: "grid grid-cols-12 gap-6 w-full") do
+              div(class: "col-span-3") do
+                a(href: href, class: "hover:underline") do
+                  render RubyUI::Heading(level: 4, class: "inline") { @presented_collection.name }
+                end
+
+                span(class: "text-muted-foreground font-normal ml-1 text-sm italic", title: "Private") {
+                  "(p)"
+                } if @collection.private
+
+                simple_format(@collection.description, class: "text-sm font-normal mt-2")
+
+                Text(size: "1", class: "text-muted-foreground mt-3 italic") do
+                  meta_info
+                end
               end
 
-              span(class: "text-muted-foreground font-normal ml-1 text-sm italic", title: "Private") {
-                "(p)"
-              } if @collection.private
-
-              simple_format(@collection.description, class: "text-sm font-normal mt-2")
-
-              Text(size: "1", class: "text-muted-foreground mt-3 italic") do
-                meta_info
-              end
-            end
-
-            div(class: "col-span-9 flex gap-6") do
-              div(class: "grid grid-cols-12 gap-9 w-full") do
-                @collection.pins.newest_first.includes(:user, pinable: [ :screenshot_attachment, url_cache: :thumb_attachment ]).limit(4).each do |pin|
-                  div(class: "col-span-3") do
-                    render Components::Pins::Pin(pin: pin)
+              div(class: "col-span-9 flex gap-6") do
+                div(class: "grid grid-cols-12 gap-9 w-full") do
+                  @collection.pins.newest_first.includes(:user, pinable: [ :screenshot_attachment, url_cache: :thumb_attachment ]).limit(4).each do |pin|
+                    div(class: "col-span-3") do
+                      render Components::Pins::Pin(pin: pin)
+                    end
                   end
                 end
               end
             end
-          end
 
-          Separator(class: "mt-6 mb-9")
+            Separator(class: "mt-6 mb-9")
+          end
         end
       end
 
