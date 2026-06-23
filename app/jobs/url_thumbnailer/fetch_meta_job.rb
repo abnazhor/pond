aea635542b720de73d35c0f256c9f7de952e5ca2
mbajur <mbajur@gmail.com>
2026-06-23 09:20:29 +0200

    Retry 429 on fetch meta job

--- app/jobs/url_thumbnailer/fetch_meta_job.rb
+++ app/jobs/url_thumbnailer/fetch_meta_job.rb
@@ -1,6 +1,8 @@
 class UrlThumbnailer::FetchMetaJob < ApplicationJob
   queue_as :fetch_url_meta
 
+  retry_on Faraday::TooManyRequestsError, wait: :polynomially_longer, attempts: 5
+
   def perform(post)
     UrlThumbnailer.new(post).call
   end
